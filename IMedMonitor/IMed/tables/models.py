3d4b7afe1fc2ff8bec0dd342451cb676c2e2c86f
karbfg10k <karthik.natraj@gmail.com>
2016-01-17 18:04:02 -0500

    First commit

---
+++ IMedMonitor/IMed/tables/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+data = [
+    {"name": "Me!"},
+    {"name": "Myself!"},
+]
+# Create your models here.
+class Person(models.Model):
+    name = models.CharField(verbose_name="full name")
+
+table = Person(data)
