94fc17fa48d9896c8744cba527d65329e2c1a409
karbfg10k <karthik.natraj@gmail.com>
2016-01-18 14:51:16 -0500

    First update

--- IMedMonitor/IMed/tables/models.py
+++ IMedMonitor/IMed/tables/models.py
@@ -6,8 +6,9 @@ data = [
     {"name": "Me!"},
     {"name": "Myself!"},
 ]
+
 # Create your models here.
 class Person(models.Model):
-    name = models.CharField(verbose_name="full name")
+    name = models.CharField(verbose_name="full name", max_length = 20)
 
 table = Person(data)
--- IMedMonitor/IMed/tables/views.py
+++ IMedMonitor/IMed/tables/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Person 
 
 
-#def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 
 def people(request):
     return render(request, "people.html", {"people": Person.objects.all()})
+    table = Person(data)
